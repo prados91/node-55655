9b53b6d5cc4c83efb859d5a322cd048b16b53ff9
Augusto <augustoprados91@gmail.com>
2023-11-28 22:11:57 -0300

    cambios

---
+++ 00_intro_js/data.js
@@ -0,0 +1,24 @@
+const myName = "Augusto";
+let age = 31;
+const price = 100;
+
+const list = ["HP","Marvel","Leo"];
+
+console.log(myName);
+
+console.log(age);
+
+console.log(price);
+
+console.log(list);
+
+//age = age+1;
+age +=1;
+console.log(age)
+age++
+
+console.log(age)
+
+list.push("PAPA") // COMO ES MUTABLE, NO PASA NADA SI ESTÁ DECLARADO COMO CONST
+
+console.log(list)
