f0a096c71f830b299fc77e43938cd5c84b92dd22
Augusto <augustoprados91@gmail.com>
2023-12-14 22:43:08 -0300

    modificaciones

--- 05_npm/server.js
+++ 05_npm/server.js
@@ -7,8 +7,8 @@ const server = http.createServer(); //metodo de http para crear un servidor.
 
 const PORT = 8080; //para iniciar el servidor necesito dos parámetros: PUERTO DONDE VA A FUNCIONAR [8080] - CALLBACK PARA INFORMAROS QUE EL SERVIDOR ESTÁ FUNCIONANDO
 const cbReady = () => {
-    console.log("Server ready on port" + PORT); // Importante mostrar en consola el puerto donde el sv está funcionando.
+    console.log("Server ready on port " + PORT); // Importante mostrar en consola el puerto donde el sv está funcionando.
     console.log("My first server");
-    console.log("Coder server")
+    console.log("Coder server");
 };
 server.listen(PORT, cbReady); //listen() -> metodo necesario para iniciar el servidor.
